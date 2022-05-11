6f04536c11157e1fd6ee89fdeb0e6d21299e0ad0
Ramakrishnan <ramakrishnanp30798@gmail.com>
2022-05-11 16:52:12 +0530

    first

--- First.java
+++ First.java
@@ -1,5 +1,8 @@
 public class First{
+  public First(int x, int y){
+    System.out.println("Welcome");
+  }
   public static void main(String args[]){
-    System.out.println("Hello");
+    System.out.println("THis is inside method");
   }
 }
--- Hello.java
+++ Hello.java
@@ -2,6 +2,11 @@ import java.util.*;
 public class Hello{
   private static Scanner scan;
 
+  public void get_numinput(){
+    scan = new Scanner(System.in);
+    
+  }
+
   public static void main(String[] args) {
     scan = new Scanner(System.in);
     String word = scan.nextLine();
